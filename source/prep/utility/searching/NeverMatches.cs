a578424d17b7ed4bc10094815f00b0175d1a4f2a
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-11-21 13:05:00 -0700

    Updated

---
+++ source/prep/utility/searching/NeverMatches.cs
@@ -0,0 +1,10 @@
+﻿namespace prep.utility.searching
+{
+  public class NeverMatches<T> : IMatchAn<T>
+  {
+    public bool matches(T item)
+    {
+      return false;
+    }
+  }
+}
--- source/prep/utility/searching/Where.cs
+++ source/prep/utility/searching/Where.cs
@@ -1,46 +1,39 @@
+using System.Collections.Generic;
+using prep.collections;
+
 namespace prep.utility.searching
 {
   public static class Where<ItemToFilter>
   {
-	  public static HasAConditionGenerator<ItemToFilter, PropertyType> has_a<PropertyType>(PropertyAccessor<ItemToFilter, PropertyType> accessor)
+    public static CriteriaFactory<ItemToFilter, PropertyType> has_a<PropertyType>(
+      PropertyAccessor<ItemToFilter, PropertyType> accessor)
     {
-		return new HasAConditionGenerator<ItemToFilter, PropertyType>(accessor);
+      return new CriteriaFactory<ItemToFilter, PropertyType>(accessor);
     }
   }
 
-	public class HasAConditionGenerator<ItemToFilter, PropertyType>
-	{
-		private PropertyAccessor<ItemToFilter, PropertyType> _accessor;
+  public class CriteriaFactory<ItemToFilter, PropertyType>
+  {
+    PropertyAccessor<ItemToFilter, PropertyType> accessor;
 
-		public HasAConditionGenerator(PropertyAccessor<ItemToFilter, PropertyType> accessor)
-		{
-			_accessor = accessor;
-		}
+    public CriteriaFactory(PropertyAccessor<ItemToFilter, PropertyType> accessor)
+    {
+      this.accessor = accessor;
+    }
 
-		public AnonymousCriteria<ItemToFilter> equal_to(PropertyType matchValue)
-		{
-			Condition<ItemToFilter> condition = (ItemToFilter value) =>
-													{
-														return object.Equals(_accessor(value), matchValue);
-													};
+    public IMatchAn<ItemToFilter> equal_to(PropertyType value)
+    {
+      return equal_to_any(value);
+    }
 
-			return new AnonymousCriteria<ItemToFilter>(condition);
-		}
+    public IMatchAn<ItemToFilter> equal_to_any(params PropertyType[] potential_values)
+    {
+      return new AnonymousCriteria<ItemToFilter>(x => new List<PropertyType>(potential_values).Contains(accessor(x)));
+    }
 
-		public AnonymousCriteria<ItemToFilter> equal_to_any(params PropertyType[] matchValues)
-		{
-			Condition<ItemToFilter> condition = (ItemToFilter value) =>
-			{
-				PropertyType accessedProperty = _accessor(value);
-				foreach(var matchValue in matchValues)
-				{
-					if (object.Equals(accessedProperty, matchValue))
-						return true;
-				}
-				return false;
-			};
-
-			return new AnonymousCriteria<ItemToFilter>(condition);
-		}
-	}
+    public IMatchAn<ItemToFilter> not_equal_to(PropertyType value)
+    {
+      throw new System.NotImplementedException();
+    }
+  }
 }
