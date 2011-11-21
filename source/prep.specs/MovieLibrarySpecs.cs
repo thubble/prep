bddbcf711856d59f425abb1cbe2ce5180384a87e
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-11-21 15:45:43 -0700

    Updated

--- source/prep.specs/MovieLibrarySpecs.cs
+++ source/prep.specs/MovieLibrarySpecs.cs
@@ -233,7 +233,7 @@ namespace prep.specs
 
       It should_be_able_to_find_all_movies_published_after_a_certain_year = () =>
       {
-        var criteria = Where<Movie>.has_a(x => x.date_published).greater_than(2004);
+        var criteria = Where<Movie>.has_a(x => x.date_published.Year).greater_than(2004);
 
         var results = sut.all_movies().all_items_matching(criteria);
 
