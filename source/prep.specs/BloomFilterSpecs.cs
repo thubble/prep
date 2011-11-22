b91f77bf97cbd830df98dec77fc5c58a041d1cc9
Tiernan Hubble <thubble@gmail.com>
2011-11-22 09:12:32 -0600

    Pushing new changes

---
+++ source/prep.specs/BloomFilterSpecs.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using Machine.Specifications;
+using developwithpassion.specifications.extensions;
+using developwithpassion.specifications.rhinomocks;
+using prep.specs.utility;
+using prep.codekata;
+
+namespace prep.specs
+{
+	[Subject(typeof(BloomFilter))]
+	public class BloomFilterSpecs
+	{
+		public abstract class bloom_filter_concern : Observes<BloomFilter>
+		{
+			protected static IList<string> words_collection;
+
+			protected static void LoadWords()
+			{
+				string fileName = @"C:\WORDLIST.TXT";
+				using (var sr = new StreamReader(fileName))
+				{
+					words_collection.Add(sr.ReadLine());
+				}
+			}
+
+			Establish c = () =>
+			{
+				words_collection = new List<string>();
+				depends.on(words_collection);
+			};
+		};
+
+		public class when_building_list_of_dictionary_words : bloom_filter_concern
+		{
+
+			Establish c = () =>
+							{
+								LoadWords();
+							};
+
+			Because b = () => 
+				result = sut.CheckWord("fkdk");
+
+			It should_iterate = () =>
+			{
+			};
+
+			static bool result;
+		}
+	}
+}
---
+++ source/prep/codekata/BloomFilter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace prep.codekata
+{
+	public class BloomFilter
+	{
+		private const int VECTOR_SIZE = 1; // size in sizeof(int) = *32
+
+
+
+		public BloomFilter(IEnumerable<string> words)
+		{
+
+		}
+
+		public bool CheckWord(string p)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public interface IHasher
+	{
+		IBitVector Hash(string str);
+	}
+
+	public interface IBitVector
+	{
+		IBitVector Or(IBitVector rightSide);
+	}
+}
